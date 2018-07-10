dddf0e4b2ebaa419e9402a2947b70f59c8c8504d
Gabriel García Seco <ggarciaseco@gmail.com>
2018-07-10 10:46:46 +0200

    test: Fix specs AlbumGrid

--- src/components/AlbumGrid/AlbumGrid.spec.js
+++ src/components/AlbumGrid/AlbumGrid.spec.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import AlbumGrid from './AlbumGrid';
+import { images } from 'containers/Home/data';
 
 describe('AlbumGrid suite', () => {
   it('should render the AlbumGrid component', () => {
-    const component = shallow(<AlbumGrid />);
+    const component = shallow(<AlbumGrid images={images.factory()} />);
     expect(component).toBeDefined();
   });
 });
